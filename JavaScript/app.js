de86e65180c2b05c11fbd4b49956e507a43bab92
Waqar Ahmed <54082156+waqarvikey75@users.noreply.github.com>
2022-05-17 22:35:19 +0500

    Side Navbar Update

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -2,4 +2,36 @@ console.log('Testing Javascript integration.');
 
 
 
-// Navbar JS code
+// Navbar side JS code
+
+
+
+
+// Side Navbar collapse Menu
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+// Collapisable button symbol change
+
+function symbol_change(symbolStatus, symbolId){
+    var symbol = document.getElementById(symbolStatus).textContent;
+    console.log(symbol);
+    console.log(symbolStatus);
+    console.log(symbolId);
+    if(symbol==='+'){
+        document.querySelector(symbolId).innerHTML = '_';
+    }else{
+        document.querySelector(symbolId).innerHTML = '+';
+    }
+}
